b7b653c272a141e43923a4b5bdc5d482bbd29a79
the-redback <maruf.2hin@gmail.com>
2017-06-30 17:17:14 +0600

    added-all

--- xorm-postgresql-go-example/main.go
+++ xorm-postgresql-go-example/main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"time"
+	"strconv"
 )
 
 const (
@@ -22,8 +23,8 @@ type User_info struct{
 	Uid int64  `xorm:"pk not null autoincr"`
 	Name string
 	Dept string
-	CreatedAt time.Time `xorm: "created"`
-	UpdatedAt time.Time `xorm: "updated"`
+	CreatedAt time.Time `xorm:"created"`
+	UpdatedAt time.Time `xorm:"updated"`
 
 }
 
@@ -36,6 +37,7 @@ type Library_info struct {
 type Book_info struct{
 	Book_id int64 `xorm:"pk not null autoincr"`
 	Author_name string
+	Version int `xorm:"version"`
 }
 
 //Structure required for joining tables
@@ -213,6 +215,147 @@ func join_User_Library(en *xorm.Engine){
 	}
 }
 
+func iterate_data(en *xorm.Engine){
+	err := en.Iterate(new(User_info), func(i int, bean interface{})error{
+		user := bean.(*User_info)
+		//do somthing use i and user
+
+		log.Println(i, *user)
+		return nil
+	})
+	if err!=nil {
+		log.Println(err)
+	}
+}
+
+func update_data(en *xorm.Engine){
+	user:=new(User_info)
+	user.Name="Abdullah Al Maruf"
+	affected,_ :=en.Id(1104029).Update(user)  //Only not null fields of user will be updated in database
+
+	log.Println("affected row: ", affected)
+
+	affected, _ = en.Id(11040029).Cols("name","dept").Update(user) //Selected collumns will be updated despite of null field
+	log.Println("affected row: ", affected)
+
+}
+
+func version_data_update(en *xorm.Engine){
+	var book Book_info
+	en.Id(100).Get(&book)
+	log.Println("Before update:",book)
+
+	en.Id(100).Update(&book)
+	en.Id(100).Get(&book)
+	log.Println("After update:",book)
+}
+
+func delete_data(en *xorm.Engine){
+	affected,err:=en.Id(11040029).Delete(&User_info{})
+	//err := en.Delete(&User_info{Name:"xlw"})
+	if err!=nil {
+		log.Println(err)
+	}
+	log.Println("affected:",affected)
+}
+
+func sql_command(en *xorm.Engine){
+	//en.Query for select, en.Exec for insert, update or delete
+	sql :="select * from user_info"
+	results,_ :=en.Query(sql)
+
+	for _, result := range results{
+		//convert results []map[string][]byte to users []UserInfo
+		var user User_info
+		user.Uid, _ =strconv.ParseInt(string(result["uid"]),10,64) //10 base, 64bit
+		user.Name=string(result["name"])
+		user.Dept=string(result["dept"])
+
+		layout := "2006-01-02T15:04:05Z"
+		user.UpdatedAt, _ = time.Parse(layout, string(result["updated_at"]))
+		user.CreatedAt, _ = time.Parse(layout, string(result["created_at"]))
+
+		log.Println(user)
+	}
+
+	//Exec for update
+	sql = "update user_info set name=? where uid=?"
+	res, _ := en.Exec(sql, "Mynul Hasan", 1104009)
+	log.Println("affected",res)
+}
+
+func sessionTransaction(en *xorm.Engine){
+	session :=en.NewSession()
+	defer session.Close()
+
+	var err error
+
+	//Begin() before starting transaction
+
+	err=session.Begin()
+
+	log.Println("Transaction begins")
+
+
+	user1 :=User_info{Name: "Maruf", Dept: "CSE"}
+	user2 :=User_info{Name: "Maruf", Dept: "CSE"}
+
+	_,err=session.Insert(&user1)
+	if err!=nil{
+		log.Println(err, "transaction rollback")
+		session.Rollback()
+		return
+	}
+	_,err=session.Insert(&user2)
+	if err!=nil{
+		log.Println(err, "transaction rollback")
+		session.Rollback()
+		return
+	}
+
+	user2.Name="Someone Else"
+	_,err= session.Where("uid=?",11040000).Update(&user2)
+	if err!=nil {
+		log.Println(err, "transaction rollback")
+		session.Rollback()
+		return
+	}
+
+	 _, err =session.Delete(&User_info{Uid: 11040000})
+	if err!= nil{
+		log.Println(err, "transaction rollback")
+		session.Rollback()
+		return
+	}
+
+	//Commit() after all actions
+	err=session.Commit()
+	log.Println("transaction successfully committed")
+
+}
+
+func events(en *xorm.Engine){
+	session := en.NewSession()
+	defer session.Close()
+
+	session.Begin()
+
+	before := func(bean interface{}){
+		fmt.Println("before", bean)
+	}
+	after := func(bean interface{}){
+		fmt.Println("after", bean)
+	}
+	fmt.Println("before Insert")
+	session.Before(before).After(after).Insert(&User_info{Name: "Sauman", Dept: "CSE"})
+	session.Before(before).After(after).Insert(&User_info{Name: "Biswas", Dept: "ME"})
+
+	fmt.Println("before commit")
+
+	session.Commit()
+
+
+}
 
 func main() {
 
@@ -223,12 +366,17 @@ func main() {
 	//insert_multiple_data(en)
 
 	//query_single_data(en)
-	query_mulltiple_data(en)
+	//query_mulltiple_data(en)
 
 	//query_conditional_data(en)
 
 	//join_User_Library(en)
-
-
+	//iterate_data(en)
+	//update_data(en)
+	//version_data_update(en)
+	//delete_data(en)
+	//sql_command(en)
+	//sessionTransaction(en)
+	//events(en)
 
 }
